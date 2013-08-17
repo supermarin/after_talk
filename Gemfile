71d88860203e517b5e510761b5e15b9299a51ceb
Marin Usalj <mneorr@gmail.com>
2013-08-17 14:46:52 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'haml'
+gem 'sinatra-contrib'
+gem 'sinatra-twitter-bootstrap'
+gem "binding_of_caller"
+gem 'sequel'
+
+group :development do
+  gem "better_errors"
+end
+
---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+require 'haml'
+require 'sinatra/twitter-bootstrap'
+require 'better_errors'
+require 'sequel'
+
+
+register Sinatra::Twitter::Bootstrap::Assets
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = __dir__
+end
+
+DB = Sequel.sqlite
+DB.create_table :reviews do
+  primary_key :id
+  String :comments
+  Int :rating
+end
+
+
+get '/' do
+  haml :survey
+end
+
+post '/review' do
+  reviews = DB[:reviews]
+  reviews.insert(:rating => params[:rating], :comments => params[:comments])
+  haml :thanks
+end
+
